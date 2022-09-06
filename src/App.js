3c0d022766cae90cc9c4a550dc84207a2fd088ee
shannone <shannone426@gmail.com>
2022-09-06 09:03:03 -0400

    side effects hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react"
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
   useEffect(() => {
     console.log("useEffect called");
   });
